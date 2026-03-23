ea38d631067e90892809636fe1a92cab101f5ad8
Martinis <natthaphong5171@gmail.com>
2026-03-23 19:00:07 +0700

    Create Download_Dataset.py

---
+++ Download_Dataset.py
@@ -0,0 +1,7 @@
+import gdown
+
+# Dataset AI Card.zip
+gdown.download("https://drive.google.com/file/d/1BPROhDA5_E90XBgOZWc6iMLUnB5taFWl/view?usp=sharing", "dataset AI Card.zip")
+
+# Dataset AI Student Performance.zip
+gdown.download("https://drive.google.com/file/d/1-Jy3kTICxqXOv-r2bO4y-Vv3nVDb1Zu9/view?usp=sharing", "Dataset AI Student Performance.zip")
